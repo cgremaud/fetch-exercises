55462b9370bb903b594baf0707974fccd2c0a755
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-10 21:53:23 -0600

    added conditionals to script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ window.addEventListener("load", () => {
     fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response) {
         response.json().then( function(json) {
             console.log(json)
-            ;
+
             let div = document.getElementById("weather-conditions")
+            let goForLaunch = false
+            
             json.windSpeed = 99
+
 
             div.innerHTML = ` <ul>
             <li>Temp ${json.temp}</li>
@@ -15,15 +18,20 @@ window.addEventListener("load", () => {
 
 
 
-         if (json.temp > 60){
+         if (json.temp > 67){
              div.innerHTML += "Temp too high <br />"
-         }
+             goForLaunch = true
+             console.log(goForLaunch)
+         } else (
+             div.innerHTML += "Temp nominal <br />"
+         )
 
          if (json.windSpeed < 50 && json.windSpeed > 2) {
             div.innerHTML += "Windspeed nominal"
          } else {
              div.innerHTML += "Windspeed outside of acceptable range"
+             
          }
-         });
+         }); 
     })
 })
