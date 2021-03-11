1e2ea17aa1b3633150c2192434730a66709147da
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-10 22:21:23 -0600

    converted to arrow funcs

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 window.addEventListener("load", () => {
-    fetch("./data.json").then(function(response) {
-        response.json().then( function(json) {
-            console.log(json)
+    fetch("https://handlers.education.launchcode.org/static/weather.json").then((response) => {
+            response.json().then((json) => {
+                console.log(json);
 
-            let div = document.getElementById("weather-conditions")
-            let goForLaunch = false
-            
-            json.windSpeed = 99
+                let div = document.getElementById("weather-conditions");
+                let goForLaunch = false;
 
-
-            div.innerHTML = ` <ul>
+                // json.windSpeed = 99
+                div.innerHTML = ` <ul>
             <li>Temp ${json.temp}</li>
             <li>Wind Speed ${json.windSpeed}</li>
             <li>Status ${json.status}</li>
             <li>Chance of Precip ${json.chanceOfPrecipitation}</li>
             <li>Zip Code ${json.zipcode}</li>
-         </ul>`
+         </ul>`;
 
+                if (json.temp > 67) {
+                    div.innerHTML += "Temp too high <br />";
+                    goForLaunch = false;
+                    console.log(goForLaunch);
+                } else {
+                    div.innerHTML += "Temp nominal <br />";
+                    goForLaunch = true;
+                }
 
+                if (json.windSpeed < 50 && json.windSpeed > 2) {
+                    div.innerHTML += "Windspeed nominal<br />";
+                    goForLaunch = true;
+                } else {
+                    div.innerHTML += "Windspeed outside of acceptable range<br />";
+                    goForLaunch = false;
+                }
 
-         if (json.temp > 67){
-             div.innerHTML += "Temp too high <br />"
-             goForLaunch = true
-             console.log(goForLaunch)
-         } else { 
-             div.innerHTML += "Temp nominal <br />"
-             goForLaunch = false
-         }
-
-         if (json.windSpeed < 50 && json.windSpeed > 2) {
-            div.innerHTML += "Windspeed nominal<br />"
-            goForLaunch = true
-         } else {
-             div.innerHTML += "Windspeed outside of acceptable range<br />"
-             goForLaunch = false
-         }
-
-         div.innerHTML += `Go For Launch: ${goForLaunch}`
-         }); 
-    })
+                div.innerHTML += `Go For Launch: ${goForLaunch}`;
+            });
+        })
 })
+
+
+//code to set promises from fetch() and .json()
+//.then indicates "do this when the response is received not before."
+// const fetchPromise = fetch("https://handlers.education.launchcode.org/static/weather.json");
+// fetchPromise.then( function(response) {
+//    const jsonPromise = response.json();
+//    jsonPromise.then( function(json) {
+//       console.log("temp", json.temp);
+//    });
+// } );
