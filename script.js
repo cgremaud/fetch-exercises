41eca22d58101bdf583363c23d37f0cdd4a4b98d
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-10 21:34:34 -0600

    messed with changing properties of json object.

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 window.addEventListener("load", () => {
     fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response) {
         response.json().then( function(json) {
-            console.log(json);
+            console.log(json)
+            ;
             let div = document.getElementById("weather-conditions")
+            json.windSpeed = 99
+
             div.innerHTML = ` <ul>
             <li>Temp ${json.temp}</li>
             <li>Wind Speed ${json.windSpeed}</li>
             <li>Status ${json.status}</li>
             <li>Chance of Precip ${json.chanceOfPrecipitation}</li>
          </ul>`
+
+
+
          if (json.temp > 60){
-             div.innerHTML += "Temp too high"
+             div.innerHTML += "Temp too high <br />"
+         }
+
+         if (json.windSpeed < 50 && json.windSpeed > 2) {
+            div.innerHTML += "Windspeed nominal"
+         } else {
+             div.innerHTML += "Windspeed outside of acceptable range"
          }
          });
     })
